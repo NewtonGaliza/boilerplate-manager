449251ac1cb21d27e2f272b75130f96ab1338bf8
Claysllan Xavier <claysllan-xavier@hotmail.com>
2018-10-25 11:12:18 -0300

    :arrow_up: Atualiza versão do pacote no Pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from setuptools import find_packages, setup
 
-VERSION = "1.0.0.dev2"
+VERSION = "1.0.1"
 AUTHOR = "Agência Municipal de Tecnologia da Informação do Município de Palmas"
 AUTHOR_EMAIL = 'dev.licencas@palmas.to.gov.br'
 
